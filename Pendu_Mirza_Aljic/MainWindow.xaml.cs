99f7b9dbcfd03ce6c5cb0d805fb7bb5de4f99bb3
Mirza-Aljic <mirzaaljic10@gmail.com>
2023-11-10 12:09:18 +0100

    Projet Pendu Mirza Aljic

--- Pendu_Mirza_Aljic/MainWindow.xaml.cs
+++ Pendu_Mirza_Aljic/MainWindow.xaml.cs
@@ -1,47 +1,48 @@
-﻿using System.Linq;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Collections.Generic;
-using System;
-using System.Text; // Ajout de l'espace de noms pour StringBuilder
-using System.IO.Packaging;
-using System.Windows.Threading;
+using System.Text;
+using System.Linq;
+using System.Windows.Media.Imaging;
 
 namespace Pendu_Mirza_Aljic
 {
     public partial class MainWindow : Window
     {
-        // Liste de mots pour le jeu du pendu
         private string[] mots = { "PROGRAMMATION", "INFORMATIQUE", "PENDU", "JOUEUR", "HANGMAN" };
-        private string motSecret; // Variable qui sert à stocker le mot que doit deviner le joueur
-        private int vies = 0; // Modifier la valeur initiale de vies à 0
-        private List<Button> lettreButtons = new List<Button>(); // Liste pour stocker les boutons du clavier. Cela permet de les activer ou désactiver
+        private string motSecret;
+        private int vies = 0;
+        private List<Button> lettreButtons = new List<Button>();
+        private Image img; // Ajout d'une variable pour l'image
+        private List<string> imagePaths = new List<string>
+        {
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\1.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\2.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\3.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\4.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\5.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\6.png",
+            "D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\7.png",
+        };
 
         public MainWindow()
         {
             InitializeComponent();
-            BTN_Start.Click += StartButton_Click;
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(120);
-            timer.Tick += timer_Tick;
-            timer.Start();
 
-            CreateAlphabetButtons();
+            BTN_Start.Click += StartButton_Click; // Associe le clic du bouton Start à la méthode StartButton_Click
+           
+            CreateAlphabetButtons(); // Crée les boutons pour chaque lettre
+            DisableAlphabetButtons(); // Désactive les boutons du clavier au démarrage
+        }
 
-            // Désactivez les boutons du clavier au départ
-            DisableAlphabetButtons();
-        }
-        void timer_Tick(object sender, EventArgs e)
-        {
-            LB_Timer.Content = DateTime.Now.ToLongTimeString();
-        }
         private void CreateAlphabetButtons()
         {
             // Crée des boutons pour chaque lettre de l'alphabet et ajoute des gestionnaires d'événements
             foreach (Button button in gridKeyboard.Children)
             {
                 lettreButtons.Add(button);
-                button.Click += KeyButton_Click;
+                button.Click += KeyButton_Click; // Associe le clic des boutons à la méthode KeyButton_Click
             }
         }
 
@@ -65,65 +66,88 @@ namespace Pendu_Mirza_Aljic
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Choix aléatoire d'un mot parmi la liste
+            StartGame();
+        }
+
+        public void StartGame()
+        {
+            // Choix aléatoire d'un mot dans la liste
             Random random = new Random();
-            motSecret = mots[random.Next(mots.Length)];
+            string newMot = mots[random.Next(mots.Length)];
 
-            // Affichage du mot sous forme de pointillés avec des espaces entre les lettres
-             TB_Mot.Text = new string('*', motSecret.Length );
+            // Assure que le nouveau mot n'est pas le même que l'ancien
+            while (newMot == motSecret)
+            {
+                newMot = mots[random.Next(mots.Length)];
+            }
+            motSecret = newMot;
 
-
-            // Réinitialisation du nombre de vies
+            // Affichage du mot sous forme de pointillés
+            TB_Mot.Text = new string('*', motSecret.Length);
             vies = 7; // Réinitialisation du nombre de vies à 7
             TB_Vie.Text = vies.ToString(); // Affichage du nombre de vies
 
-            // Activation des boutons du clavier
-            EnableAlphabetButtons();
+            EnableAlphabetButtons(); // Active les boutons du clavier
+
+ 
+            // Après avoir instancié l'objet img avec le chemin d'image
+            Image.Source = new BitmapImage(new Uri("D:\\Mirza ALJIC\\SUBRAMANI\\C#\\Prrojet Jeu Pendu\\Pendu_Mirza_Aljic\\Ressource\\1.png"));
+
         }
 
         private void KeyButton_Click(object sender, RoutedEventArgs e)
         {
+            // Gestion du clic des boutons clavier
             Button button = (Button)sender;
             char cont = button.Content.ToString()[0];
 
-            verification(cont);
-            // Désactivez le bouton du clavier après utilisation
-            button.IsEnabled = false;
+            verification(cont); // Vérifie si la lettre est dans le mot
+            button.IsEnabled = false; // Désactive le bouton après utilisation
         }
+      public void verification(char lettre)
+{
+    if (motSecret.Contains(lettre))
+    {
+        MettreAJourMotAvecLettre(lettre); // Met à jour le mot avec la lettre
 
-        public void verification(char lettre )
+        if (!TB_Mot.Text.Contains("*"))
         {
-
-            if (motSecret.Contains(lettre))
-            {
-                MettreAJourMotAvecLettre(lettre); // Utilisation de la nouvelle fonction
-
-                // Vérifiez si le joueur a gagné
-                if (!TB_Mot.Text.Contains("*"))
-                {
-                    MessageBox.Show("Félicitations, vous avez deviné le mot : " + motSecret);
-                    DisableAlphabetButtons();
-                }
-            }
-            else
-            {
-                // Réduction du nombre de vies
-                vies--;
-                TB_Vie.Text = vies.ToString();
-
-                // Vérifiez si le joueur a perdu
-                if (vies == 0)
-                {
-                    MessageBox.Show("Désolé, vous avez perdu. Le mot était : " + motSecret);
-                    DisableAlphabetButtons();
-                }
-
-
-            }
+            MessageBox.Show("Félicitations, vous avez deviné le mot : " + motSecret);
+            DisableAlphabetButtons(); // Désactive les boutons après avoir trouvé le mot
         }
+    }
+    else
+    {
+        vies--; // Réduction du nombre de vies
+        TB_Vie.Text = vies.ToString(); // Met à jour l'affichage des vies
+
+        // Charger l'image ici
+        if (vies >= 0 && vies < imagePaths.Count)
+        {
+            // Supprimer l'image précédente s'il y en a une
+            if (GridKeyboardImage.Children.Count > 1)
+            {
+                GridKeyboardImage.Children.RemoveAt(1);
+            }
+            
+            img = new Image();
+            img.Source = new BitmapImage(new Uri(imagePaths[imagePaths.Count - 1 - vies]));
+            // Ajouter la nouvelle image à votre interface
+            GridKeyboardImage.Children.Add(img);
+            Grid.SetColumn(img, 1); // Pour positionner l'image
+            Grid.SetRow(img, 0); // Pour positionner l'image
+        }
+        
+        if (vies == 0) {
+            MessageBox.Show("Désolé, vous avez perdu ! Le mot était : " + motSecret);
+            DisableAlphabetButtons(); // Désactiver le clavier
+        }
+    }
+}
 
         private void MettreAJourMotAvecLettre(char lettre)
         {
+            // Met à jour le mot avec la lettre trouvée
             StringBuilder nouveauMot = new StringBuilder(TB_Mot.Text);
 
             for (int i = 0; i < nouveauMot.Length; i++)
@@ -135,6 +159,70 @@ namespace Pendu_Mirza_Aljic
             }
 
             TB_Mot.Text = nouveauMot.ToString();
+        }
+
+        private void BTN_Indice_Click(object sender, RoutedEventArgs e)
+        {
+            // Affiche un message d'indice en fonction du mot
+            if (!string.IsNullOrEmpty(motSecret))
+            {
+                string message = string.Empty;
+
+                if (motSecret == "PROGRAMMATION")
+                {
+                    message = "C'est un terme lié à l'informatique.";
+                }
+                else if (motSecret == "INFORMATIQUE")
+                {
+                    message = "Cela concerne le traitement de l'information.";
+                }
+                else if (motSecret == "PENDU")
+                {
+                    message = "Le jeu que vous jouez en ce moment.";
+                }
+                else if (motSecret == "JOUEUR")
+                {
+                    message = "Vous êtes le joueur actuel.";
+                }
+                else if (motSecret == "HANGMAN")
+                {
+                    message = "Un autre nom pour le jeu du pendu.";
+                }
+                else
+                {
+                    message = "Aucun message disponible pour ce mot.";
+                }
+
+                MessageBox.Show(message); // Affiche le message
+            }
+            else
+            {
+                MessageBox.Show("Veuillez commencer le jeu en appuyant sur le bouton 'Start'.");
+            }
+        }
+
+        private void BTN_Continue_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+            //ChangeMotSecret(); // Change le mot secret
+            //EnableAlphabetButtons(); // Active les boutons du clavier
+            //TB_Mot.Text = new string('*', motSecret.Length); // Réinitialise l'affichage du mot
+            //vies = 7; // Réinitialisation du nombre de vies à 7
+            //TB_Vie.Text = vies.ToString(); // Affichage du nombre de vies
+
+        }
+
+        private void ChangeMotSecret()
+        {
+            // Change le mot secret en choisissant un nouveau mot aléatoire
+            Random random = new Random();
+            string newMot = mots[random.Next(mots.Length)];
+
+            while (newMot == motSecret)
+            {
+                newMot = mots[random.Next(mots.Length)];
+            }
+            motSecret = newMot;
         }
     }
 }
